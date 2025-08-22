ee4e166963e02a42de6575bf02e1fac3cf7af5d2
halky <yuyami.stars@gmail.com>
2025-08-23 07:48:08 +0900

    pages-reflection: リファクタリングと、FormLayoutへの置き換え

--- Nijo.ApplicationTemplate.Ver1/react/src/pages-reflection/SingleView.tsx
+++ Nijo.ApplicationTemplate.Ver1/react/src/pages-reflection/SingleView.tsx
@@ -82,7 +82,6 @@ export const SingleView = <
         </Layout.PageFrameTitle>
       )}
       className="p-4 relative"
-      style={SINGLE_VIEW_STYLE}
     >
       {nowLoading && (
         <Layout.NowLoading />
@@ -125,8 +124,4 @@ const AfterLoaded = <
       formMethods={form as unknown as ReactHookForm.UseFormReturn<ReactHookForm.FieldValues>}
     />
   )
-}
-
-const SINGLE_VIEW_STYLE: React.CSSProperties = {
-  fontFamily: '"ＭＳ 明朝", sans-serif',
 }
