7b274370ccd70fe653d70b32ea79850a5065a03a
halky <yuyami.stars@gmail.com>
2025-08-30 18:50:55 +0900

    古いソースを削除

--- Xml設定GUI/src/ver1/ui-components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Accordion"
-export * from "./DataTable2"
-export * from "./DataTable2.useColumnDef"
-export * from "./MarkdownTextarea"
-export * from "./Outliner"
-export * from "./PageFrame"
-export * from "./SectionTitle"
