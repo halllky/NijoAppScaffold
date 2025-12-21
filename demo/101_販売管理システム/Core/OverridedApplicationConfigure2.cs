cfb1222faba942463b2461f2752a4aff9f3ae600
halky <yuyami.stars@gmail.com>
2025-12-21 13:17:42 +0900

    デモ101 fix

--- demo/101_販売管理システム/Core/OverridedApplicationConfigure2.cs
+++ demo/101_販売管理システム/Core/OverridedApplicationConfigure2.cs
@@ -16,4 +16,14 @@ partial class OverridedApplicationConfigure {
             services.AddTransient<I商品管理システム, 商品管理システム本番>();
         }
     }
+
+    protected override void ConfigureSequenceMember(
+        Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder,
+        Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder entity,
+        Microsoft.EntityFrameworkCore.Metadata.Builders.PropertyBuilder<int?> property,
+        string sequenceName) {
+
+        // SQLite の場合（SQLiteにはシーケンスがないため、AUTO_INCREMENTを使用）
+        property.HasAnnotation("Sqlite:Autoincrement", true);
+    }
 }
