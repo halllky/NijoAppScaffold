49efbc955272d55322c9b0cecf42b73910de5824
halky <yuyami.stars@gmail.com>
2026-06-13 22:12:20 +0000

    doc: ページ構成を整理

--- Document/src/pages/index.tsx
+++ Document/src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 import { Redirect } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-// 「はじめに」(docs/01_intro) へリダイレクト
+// 「はじめに」へリダイレクト
 export default function Home(): ReactNode {
   return <Redirect to={useBaseUrl('/docs/intro')} />;
 }
